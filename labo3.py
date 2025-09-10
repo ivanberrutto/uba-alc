ab1cc1062a3e0ba4dc0319e5365e926771242ddf
Ivan Gonzalo Berrutto <ivanberrutto@gmail.com>
2025-09-10 01:25:31 -0300

    resuelvo ejercicio 1 laboratorio 3

--- labo3.py
+++ labo3.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-def norma(x,y):
+def norma(x,p):
+    if(p=='inf'):
+        mayor = 0
+        for i in x:
+            mayor = max(abs(i),mayor)
+        return mayor
+            
     res = 0
     for i in x:
-        res = res + (abs(i) ** y) 
-    res = res ** (1/y)
+        res = res + (abs(i) ** p) 
+    res = res ** (1/p)
     return res
 
 assert(np.allclose(norma(np.array([1, 1]), 2), np.sqrt(2)))
@@ -13,14 +19,15 @@ assert(norma(np.random.rand(10), 2) <= np.sqrt(10))
 assert(norma(np.random.rand(10), 2) >= 0)
 
 def normaliza(lista,p):
-    res = np.array
+    res = []
     i = 0
     for v in lista:
-        res[i] = norma(lista[i],p)
-        i = i+1
+        res.append( v / norma(v,p) )
     return res
 
 
-assert all(np.allclose(norma(x, 2), 1) for x in normaliza([np.array([1] * k) for k in range(1, 11)], 2))
-assert all(not np.allclose(norma(x, 2), 1) for x in normaliza([np.array([1] * k) for k in range(1, 11)], 1))
-assert all(np.allclose(norma(x, np.inf), 1) for x in normaliza([np.random.rand(k) for k in range(1, 11)], np.inf))
+assert (np.allclose(norma(x, 2), 1) for x in normaliza([np.array([1] * k) for k in range(1, 11)], 2))
+assert ([not np.allclose( norma ( x , 2 ) , 1 ) for x in normaliza([ np.array([1]*k) for k in range(1, 11 ) ] , 1) ])
+assert (np.allclose(norma(x, np.inf), 1) for x in normaliza([np.random.rand(k) for k in range(1, 11)], np.inf))
+
+
